46ae80facfb6b458e6d327e856c500dad65cca7a
fsaii <fusaila_kiku@hotmail.com>
2025-04-20 16:20:03 +0700

    Add files via upload

--- app.py
+++ app.py
@@ -30,3 +30,14 @@ X, _ = make_blobs(n_samples=300, centers=loaded_model.n_clusters, cluster_std=0.
 
 #predict using the loaded model
 y_kmeans = loaded_model.predict(X)
+
+# Plotting
+fig, ax = plt.subplots()
+scatter = ax.scatter(X[:, 0], X[:, 1], c=y_kmeans, cmap='viridis')
+centers = loaded_model.cluster_centers_
+ax.scatter(centers[:, 0], centers[:, 1], c='red', s=200, alpha=0.75, label='Centroids')
+
+ax.set_title('k-Means Clustering')
+ax.legend()
+
+st.pyplot(fig)
